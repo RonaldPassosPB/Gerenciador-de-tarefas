d871997089735d840e4db487a5825d89d50ccf29
RonaldPassosPB <ronaldpassosft@gmail.com>
2025-01-08 18:30:26 -0300

    initial project

---
+++ src/Pages/TaskPage.jsx
@@ -0,0 +1,32 @@
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import Title from "../components/Title";
+
+function TaskPage() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+  return (
+    <div className="h-screen w-screen bg-slate-500 p-6">
+      <div className="w-[500px] mx-auto space-y-4">
+        <div className="flex justify-center relative mb-6">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <Title>Detalhes da Tarefa</Title>
+        </div>
+
+        <div className="bg-slate-400 p-4 rounded-md">
+          <h2 className="text-xl text-white font-bold">{title}</h2>
+          <p className="text-white">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TaskPage;
